80c5932ceee6163ea592ffd334d598162c647ebc
Ravi <raview69@coding.lan>
2024-05-01 18:35:39 +0700

    Fix; flow open and close order

--- main.py
+++ main.py
@@ -22,62 +22,80 @@ action = ActionChains(driver)
 url = 'https://www.binance.com/en/futures/NEARUSDT'
 driver.get(url)
 driver.maximize_window()
-sleep(10)
+sleep(40)
 # openPrice = driver.find_element('xpath', '//*[@id="futuresOrderbook"]/div[3]/div[2]/div[1]').text
 
-# # driver.find_element('xpath', '//*[@id="server-side"]/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[3]/div[1]/div/div/div[9]').click()
+
 # action.double_click(driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[1]/div[1]/div/div/input')).perform()
 # # driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[1]/div[1]/div/div/input').click()
 # # toClear.send_keys(Keys.CONTROL + "a")
 # # toClear.send_keys(Keys.DELETE)
 # driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[1]/div[1]/div/div/input').send_keys(87232)
 # print(openPrice, 'price')
-# # driver.find_element('xpath', '//*[@id="server-side"]/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[2]/div[2]/div/button[1]').click()
+
 # print('finish')
 # sleep(20)
-j=0
+
+
+
+
+# for i in range(0, 2):
+#   try:
+#     driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[7]/div[2]/div/div[1]/div[1]/div').click()
+#     driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[7]/div[2]/div/div[2]/div/div/div/div/div/div[3]/div[1]/div/div/div/div/div[10]/div/button[1]').click()
+#     sleep(5)
+#     print('position closed successfully')
+#   except:
+#     print('not called')
+
+
+
+
 for i in range(0, position):
     while True:
           price = driver.find_element('xpath', '//*[@id="futuresOrderbook"]/div[3]/div[2]/div[1]').text
-          print("i", i)
-          if float(price) >= 7.180:
+          driver.find_element('xpath', '//*[@id="futuresOrderbook"]/div[3]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[1]').click()
+          #error disini setelah sukses close order
+          driver.find_element('xpath', '//*[@id="server-side"]/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[3]/div[1]/div/div/div[9]').click()
+          driver.find_element('xpath', '//*[@id="server-side"]/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[2]/div[2]/div/button[1]').click()
+          isOpen = driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[7]/div[2]/div/div[1]/div[1]').text
+          j=0
+          if isOpen != "Positions(0)":
               while j < 10000:
                   price2 = driver.find_element('xpath', '//*[@id="futuresOrderbook"]/div[3]/div[2]/div[1]').text
                   data.append(price2)
                   print(data)
                   print("j", j)
                   if(len(data) > 1):
-                      #masih bug ketika habis cutloss/ clear array
                       if(data[j - 1] > data[j]):
                           print(colored('Price is decreasing', 'red'))
+                          ## bugg for lose order
+                          driver.find_element('css selector', '#tab-POSITIONS').click()
+                          sleep(1)
+                          driver.find_element('css selector', '#position-pc > div > div.list-container.css-eaerhv > div.list-auto-sizer > div > div > div > div > div.closePosition > div > button.css-17ech4f').click()
+                          sleep(20)
                           data.clear()
                           break
                       if(data[j - 1] == data[j]):
                           print(colored('Price is relatively stable', 'yellow'))
                       if(data[j - 1] < data[j]):
                           print(colored('Price is increasing', 'green'))
+                          sleep(20)
                   j += 1
-                  sleep(5)
-               
+                  sleep(20)
               print('Price morethan', price)
               break
           else:
-              print('Price is less than', price)
-              sleep(5)
-
-    # price = driver.find_element('xpath', '//*[@id="futuresOrderbook"]/div[3]/div[2]/div[1]').text
-    # data.append(price)
-    # # len(data)
-    # # print(data)
-    # # print(len(data))
-    # print(i+1, 'Price:', price)
-    # if(len(data) > 1):
-    #     if(data[i - 1] > data[i]):
-    #         print(colored('Price is decreasing', 'red'))
-    #     if(data[i - 1] == data[i]):
-    #         print(colored('Price is relatively stable', 'yellow'))
-    #     if(data[i - 1] < data[i]):
-    #         print(colored('Price is increasing', 'green'))
-    
+            try:
+               driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[7]/div[2]/div/div[1]/div[2]').click()
+               driver.find_element('css selector', '#OPEN_ORDERS > div > div > div > div.list-container.css-eaerhv > div.list-auto-sizer > div > div > div > div > div.action.css-10nf7hq > svg').click()
+               sleep(5)
+               driver.find_element('xpath', '/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div[1]/div/div[4]/div[2]/form/div/div[1]/div[3]/div[1]/div/div/div[5]').click()
+               print('position closed successfully')
+               sleep(5)
+            except:
+                print('not called')
+                sleep(5)
+                print('Price is less than', price)
     sleep(10)
 
